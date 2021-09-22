c9eb5d962731bd834832c8251e5d3a832ca8ce72
John Paul Depew <jpdepewjp@gmail.com>
2021-09-21 21:36:09 -0400

    testing video player controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReactPlayer from "react-player";
 
 function App() {
+  const videoRef = useRef<any>(null);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        const curVidRef = videoRef?.current;
+        curVidRef && curVidRef.seekTo(curVidRef.getCurrentTime() + 1 / 60);
+      }
+      if (e.key === "ArrowLeft") {
+        const curVidRef = videoRef?.current;
+        curVidRef && curVidRef.seekTo(curVidRef.getCurrentTime() - 1 / 60);
+      }
+    });
+    return () => {
+      document.removeEventListener("keydown", (e) => e);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,13 +29,24 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=7CwT4q58N54"
+          ref={videoRef}
+          config={{
+            youtube: {
+              playerVars: {
+                controls: 1,
+              },
+            },
+          }}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Today
         </a>
       </header>
     </div>
